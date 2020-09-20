1c3dd5635437c49ab39cc5c2343ff1f1daaf8353
liubo <1091643978@qq.com>
2020-09-20 13:02:55 +0800

    sf

--- src/main/java/ExampleIf03.java
+++ src/main/java/ExampleIf03.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.net.URL;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
@@ -20,21 +19,19 @@ public class ExampleIf03 {
 
     public static void main(String[] args) throws IOException {
         ImportParams params = new ImportParams();
-//        params.setTitleRows(1);
         params.setHeadRows(1);
-        long start = new Date().getTime();
+        long start = System.currentTimeMillis();
         URL resource = ExampleIf03.class.getResource("/register_status.xlsx");
         String file = resource.getFile();
         List<Message> messages = ExcelImportUtil.importExcel(
-                new File(file),  Message.class, params);
-        System.out.println(new Date().getTime() - start);
+                new File(file), Message.class, params);
+        System.out.println(System.currentTimeMillis() - start);
         System.out.println(messages.size());
         System.out.println(ReflectionToStringBuilder.toString(messages.get(0)));
 
 
-
         HashMap<String, List<Message>> scopes = new HashMap<String, List<Message>>();
-        scopes.put("messages",messages);
+        scopes.put("messages", messages);
 
         Writer writer = new OutputStreamWriter(System.out);
         MustacheFactory mf = new DefaultMustacheFactory();
